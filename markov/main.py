9dd000c9db9b36d9e2ee6e480e4910630ded78e0
dhalima3 <dhalima@gatech.edu>
2016-01-23 20:32:09 -0500

    update main driver

--- markov/main.py
+++ markov/main.py
@@ -2,39 +2,6 @@ import pysynth
 from MusicMatrix import MusicMatrix
 
 musicLearner = MusicMatrix()
-musicLearner.add(["c", 4])
-musicLearner.add(["c", 4])
-musicLearner.add(["c", 4])
-musicLearner.add(["d", 8])
-musicLearner.add(["e", 4])
-musicLearner.add(["e", 4])
-musicLearner.add(["d", 8])
-musicLearner.add(["e", 4])
-musicLearner.add(["f", 8])
-musicLearner.add(["g", 2])
-
-musicLearner.add(["c", 8])
-musicLearner.add(["c", 8])
-musicLearner.add(["c", 8])
-
-musicLearner.add(["g", 8])
-musicLearner.add(["g", 8])
-musicLearner.add(["g", 8])
-
-musicLearner.add(["e", 8])
-musicLearner.add(["e", 8])
-musicLearner.add(["e", 8])
-
-musicLearner.add(["c", 8])
-musicLearner.add(["c", 8])
-musicLearner.add(["c", 8])
-
-musicLearner.add(["g", 4])
-musicLearner.add(["f", 8])
-musicLearner.add(["e", 4])
-musicLearner.add(["d", 8])
-musicLearner.add(["c", 2])
-
 random_score = []
 current_note = ["c", 4]
 for i in range(0, 100):
